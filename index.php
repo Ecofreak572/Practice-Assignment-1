cc78b2c193a777548c7dd62137be67b59d226983
Ecofreak572 <69659517+Ecofreak572@users.noreply.github.com>
2022-02-16 14:49:55 -0800

    Update index.php

--- index.php
+++ index.php
@@ -66,12 +66,22 @@
         </div>
       </div>
       <hr>
-        <div id="skills" class="content-item">
-            <div class="vertical-center">
-                <h2>Skills</h2>
-                <p>...</p>
+      <div id="skills" class="content-item">
+        <div class="vertical-center">
+            <h2>Skills</h2>
+            <p>...</p>
+        </div>
+      </div>
+      <hr>
+      <div id="interests" class="content-item">
+        <div class="vertical-center">
+            <h2>Interests</h2>
+            <p...</p>
+        </div>
+      </div>
+      <hr>
     </div>
-
+      
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
   </body>
 </html>
