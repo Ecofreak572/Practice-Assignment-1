749dd3390660b9b0645b3def7cde5d2ed9cf6ba3
Ecofreak572 <69659517+Ecofreak572@users.noreply.github.com>
2022-02-16 14:43:01 -0800

    Update index.php

--- index.php
+++ index.php
@@ -66,6 +66,10 @@
         </div>
       </div>
       <hr>
+        <div id="skills" class="content-item">
+            <div class="vertical-center">
+                <h2>Skills</h2>
+                <p>...</p>
     </div>
 
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
